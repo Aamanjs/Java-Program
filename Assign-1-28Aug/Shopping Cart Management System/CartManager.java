2a9c689570291c8699666b06d59fa07535006f31
Aaman Sayyad <86286688+Aamanjs@users.noreply.github.com>
2025-08-31 12:01:24 +0530

    CartManager.java

--- Assign-1-28Aug/Shopping Cart Management System/CartManager.java
+++ Assign-1-28Aug/Shopping Cart Management System/CartManager.java
@@ -1,3 +1,4 @@
+package shoppingCart;
 
 import java.util.Scanner;
 
@@ -24,14 +25,72 @@ public class CartManager {
     	return new Product(s.nextInt(), s.next(), s.nextInt(), s.nextInt());
     }
     
-    void displayProduct(Product[] p)
+    public Product updateProduct(Product[] products)
+    {
+    	Scanner s = new Scanner(System.in);
+    	System.out.println("Enter productId to update:");
+    	int id = s.nextInt();
+    	
+    	boolean found = false;
+    	for(Product p: products) {
+    		if(p.productid == id) {
+    			found = true;
+    			System.out.println("What do you want to update? (1: Price, 2: Quantity)");
+    			int choice = s.nextInt();
+    			if (choice == 1) {
+    				System.out.println("Enter new price:");
+    				p.price = s.nextInt();
+    				System.out.println("Price updated successfully!");
+    			} else if (choice == 2) {
+    				System.out.println("Enter new quantity:");
+    				p.quantity = s.nextInt();
+    				System.out.println("Quantity updated succeasfully");
+    			} else {
+    				System.out.println("Invalid choice!");
+    			}
+    			return p;
+    		}
+    	}
+    	
+    	System.out.println("Product with ID " + id + " not found!");
+    	return null;
+    }
+    
+//    public Product deleteProducts(Product[] products)
+//    {
+//    	Scanner s = new Scanner(System.in);
+//    	System.out.println("Enter productId to update:");
+//    	int id = s.nextInt();
+//    	
+//    	for(Product p: products) {
+//    		if(p.productid == id) {
+//    			p[i] -= p[i].productid;
+//    		}
+//    		
+//    	}
+//
+//    	
+//    }
+//    
+    
+    public int getTotalBill(Product[] products) {
+    	int totalBill = 0;
+    	for (Product p : products)
+    	{
+    		totalBill += p.price * p.quantity;
+    	}
+    	
+    	return totalBill;
+    }
+    
+    void viewProduct(Product[] p)
     {
     	for(int i=0; i<p.length; i++)
         {
-            p[i].displayProduct();     
-            System.out.println("Total products created: " + Product.productCount); 
+            p[i].viewProduct();     
         }
     }
         
     
 }
+
