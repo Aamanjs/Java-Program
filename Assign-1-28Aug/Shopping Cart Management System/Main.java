4402e7afd7697fb81895d1c82571c86b6ca844d8
Aaman Sayyad <86286688+Aamanjs@users.noreply.github.com>
2025-08-31 12:02:21 +0530

    Main.java

--- Assign-1-28Aug/Shopping Cart Management System/Main.java
+++ Assign-1-28Aug/Shopping Cart Management System/Main.java
@@ -1,12 +1,34 @@
 
+package shoppingCart;
+
 public class Main {
 
     public static void main(String[] args) {
         CartManager cm = new CartManager();
-
+        
         Product[] p = cm.createproduct();
         
-        cm.displayProduct(p);
+        cm.viewProduct(p);
+        System.out.println("Total products created: " + Product.productCount); 
+        
+        Product newProduct = cm.addProduct();
+        
+        Product[] up = new Product[p.length + 1];
+        for( int i = 0; i<p.length; i++)
+        {
+        	up[i] = p[i];
+        }
+        
+        up[p.length] = newProduct;
+        
+        cm.updateProduct(up);
+        System.out.println("\n--- Updated Product List ---");
+        
+        cm.viewProduct(up);
+        System.out.println("Total products created: " + Product.productCount);
+        
+        System.out.println("\nTotal Bill: " + cm.getTotalBill(up)); 
+        	
     }
     
 }
