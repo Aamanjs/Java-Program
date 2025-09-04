d7b09e8d3233b1e9148d6bacc420689807cec9ca
Aaman Sayyad <86286688+Aamanjs@users.noreply.github.com>
2025-09-04 10:43:47 +0530

    Update ShapeCalculator.java

--- Polymorphism/Method Overloading/Shape Area Calculator/ShapeCalculator.java
+++ Polymorphism/Method Overloading/Shape Area Calculator/ShapeCalculator.java
@@ -23,4 +23,29 @@ class ShapeCalculator {
         }
     }
 
+}package shapeCalculator;
+
+class ShapeCalculator {
+
+    int area(int side){
+        return side*4;
+    }
+
+    int area(int length, int breadth){
+        return length*breadth;
+    }
+
+    double area(double radius){
+        return 3.14*radius*radius;
+    }
+
+    double area(double base, double height, boolean isTriangle){
+        if(isTriangle){
+        return 0.5*base*height;
+        } else {
+            System.out.println("Not a triangle! Returning 0");
+            return 0;
+        }
+    }
+
 }
